afb1117358468add405af5c9fb9255e30208fb05
mdioc <oss@znta.io>
2025-01-13 20:37:03 -0500

    cleanup build.zig

--- build.zig
+++ build.zig
@@ -4,16 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{ .name = "dutils", .root_source_file = b.path("src/root.zig"), .target = target, .optimize = optimize });
-
     _ = b.addModule("dutils", .{ .root_source_file = b.path("src/main.zig") });
-
-    b.installArtifact(exe);
-
-    const run_exe = b.addRunArtifact(exe);
-
-    const run_step = b.step("run", "Run the application");
-    run_step.dependOn(&run_exe.step);
 
     const unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
